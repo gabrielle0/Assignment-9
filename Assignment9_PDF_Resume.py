16f8686c42c911a667f33abbd6091563faf373d0
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2022-02-07 21:26:34 +0800

    added code for pic

--- Assignment9_PDF_Resume.py
+++ Assignment9_PDF_Resume.py
@@ -45,63 +45,85 @@ org1 = jsonData.get("academic orgs")[0]
 org2 = jsonData.get("academic orgs")[1]
 
 
+class PDF(FPDF):
+	def header (self):
+		self.image("resume_pic.png", 158, 15, 40)
 
-pdf = FPDF("P", "mm", "Letter")
+
+pdf = PDF("P", "mm", "Letter")
 pdf.add_page()
 
 
-pdf.set_font("helvetica", "B")
-pdf.set_font_size (14)
-pdf.cell(40, 10, f"{heading1}", ln=True)
+def type_personal_information ():
+	pdf.set_font("helvetica", "B")
+	pdf.set_font_size (14)
+	pdf.cell(40, 10, f"{heading1}", ln=True)
+	pdf.set_font("helvetica", "I")
+	pdf.set_font_size (12)
+	pdf.cell (40, 8, f"{name}", ln=True)
+	pdf.cell (40, 8, f"{address}", ln=True)
+	pdf.cell (40, 8, f"{contact}", ln=True)
+	pdf.cell (40, 8, f"{course}", ln=True)
+	
+def type_profile ():
+	pdf.set_font("helvetica", "B")
+	pdf.set_font_size (14)
+	pdf.cell(40, 15, f"{heading2}", ln=True)
+	pdf.set_font("helvetica")
+	pdf.set_font_size (12)
+	pdf.cell(40, 8, f"{profile}", ln=True)
+		
+def type_skills ():
+	pdf.set_font("helvetica", "B")
+	pdf.set_font_size (14)
+	pdf.cell(40, 15, f"{heading3}", ln=True)
 
-pdf.set_font("helvetica", "I")
-pdf.set_font_size (12)
-pdf.cell (40, 8, f"{name}", ln=True)
-pdf.cell (40, 8, f"{address}", ln=True)
-pdf.cell (40, 8, f"{contact}", ln=True)
-pdf.cell (40, 8, f"{course}", ln=True)
+	pdf.set_font("helvetica")
+	pdf.set_font_size (12)
+	pdf.cell(40, 8, f"{skill1}", ln=True)
+	pdf.cell (40, 8, f"{skill2}", ln=True)
+	pdf.cell (40, 8, f"{skill3}", ln=True)
+	pdf.cell (40, 8, f"{skill4}", ln=True)
+
+def type_school():
+	pdf.set_font("helvetica", "B")
+	pdf.set_font_size (14)
+	pdf.cell(40, 15, f"{heading4}", ln=True)
+
+	pdf.set_font("helvetica")
+	pdf.set_font_size (12)
+	pdf.cell(40, 8, f"{school1}", ln=True)
+	pdf.cell (40, 8, f"{school2}", ln=True)
+	pdf.cell (40, 8, f"{school3}", ln=True)
+	pdf.cell (40, 8, f"{school4}", ln=True)
+
+def type_orgs():
+	pdf.set_font("helvetica", "B")
+	pdf.set_font_size (14)
+	pdf.cell(40, 15, f"{heading5}", ln=True)
+
+	pdf.set_font("helvetica")
+	pdf.set_font_size (12)
+	pdf.cell(40, 8, f"{org1}", ln=True)
+	pdf.cell (40, 8, f"{org2}", ln=True)
 
 
-pdf.set_font("helvetica", "B")
-pdf.set_font_size (14)
-pdf.cell(40, 15, f"{heading2}", ln=True)
-
-pdf.set_font("helvetica")
-pdf.set_font_size (12)
-pdf.cell(40, 8, f"{profile}", ln=True)
-
-pdf.set_font("helvetica", "B")
-pdf.set_font_size (14)
-pdf.cell(40, 15, f"{heading3}", ln=True)
-
-pdf.set_font("helvetica")
-pdf.set_font_size (12)
-pdf.cell(40, 8, f"{skill1}", ln=True)
-pdf.cell (40, 8, f"{skill2}", ln=True)
-pdf.cell (40, 8, f"{skill3}", ln=True)
-pdf.cell (40, 8, f"{skill4}", ln=True)
-
-pdf.set_font("helvetica", "B")
-pdf.set_font_size (14)
-pdf.cell(40, 15, f"{heading4}", ln=True)
-
-pdf.set_font("helvetica")
-pdf.set_font_size (12)
-pdf.cell(40, 8, f"{school1}", ln=True)
-pdf.cell (40, 8, f"{school2}", ln=True)
-pdf.cell (40, 8, f"{school3}", ln=True)
-pdf.cell (40, 8, f"{school4}", ln=True)
-
-pdf.set_font("helvetica", "B")
-pdf.set_font_size (14)
-pdf.cell(40, 15, f"{heading5}", ln=True)
-
-pdf.set_font("helvetica")
-pdf.set_font_size (12)
-pdf.cell(40, 8, f"{org1}", ln=True)
-pdf.cell (40, 8, f"{org2}", ln=True)
+type_personal_information()
+type_profile()
+type_skills()
+type_school()
+type_orgs()
 
 
 pdf.output("PANGILINAN_GABRIELLE.pdf")
 print("To get PDF, type: python [full name of document] in terminal (ex: python Assignment9_PDF_Resume.py)")
+
+
+
+
+
+
+
+
+
 
