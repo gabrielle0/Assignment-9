69e348fa044b0a3f46ce59aa00e09d11e5fd5884
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2022-02-07 16:37:33 +0800

    added code for opening json file and converting to dict

--- Assignment9_PDF_Resume.py
+++ Assignment9_PDF_Resume.py
@@ -10,18 +10,68 @@
 	#- Search how to read JSON file
 	#- You will create the JSON file manually
 
+from fpdf import FPDF
+
 import json
 
-with open("Resume.json", "r") as file:
-    jsonData = json.load(file)
-print("Datatype of variable: ", type(jsonData))
-for i in jsonData:
-    print(i, jsonData[i])
+filename = input("Enter filename (ex: Resume.json): ")
+data =  open(filename)
+jsonData = json.load(data)
+
+#format for calling key and array
+print (jsonData.get("name"))
+print (jsonData.get("skills")[0])
+
+
+heading1 = jsonData.get("heading1")
+name = jsonData.get("name")
+address = jsonData.get("address")
+contact = jsonData.get("contact information")
+course = jsonData.get("course")
+
+heading2 = jsonData.get("heading2")
+profile = jsonData.get("profile")
+
+heading3 = jsonData.get("skills")
+skill1 = jsonData.get("skills")[0]
+skill2 = jsonData.get("skills")[1]
+skill3 = jsonData.get("skills")[2]
+skill4 = jsonData.get("skills")[3]
+
+heading4 = jsonData.get("heading4")
+school1 = jsonData.get("education")[0]
+school2 = jsonData.get("education")[1]
+school3 = jsonData.get("education")[2]
+school4 = jsonData.get("education")[3]
+
+heading5 = jsonData.get("heading5")
+org1 = jsonData.get("academic orgs")[0]
+org2 = jsonData.get("academic orgs")[1]
+
+
+
+pdf = FPDF("P", "mm", "Letter")
+pdf.add_page()
+
+pdf.set_font("times", "B")
+pdf.set_font_size (15)
+pdf.cell(40, 10, f"{heading1}", ln=2)
+
+pdf.set_font("times")
+pdf.set_font_size (13)
+pdf.cell (40, 10, f"{name}", ln=1)
+pdf.cell (40, 10, f"{address}", ln=1)
+pdf.cell (40, 10, f"{contact}", ln=1)
+pdf.cell (40, 10, f"{course}", ln=3)
+
+
+pdf.set_font("times", "B")
+pdf.set_font_size (15)
+pdf.cell(40, 10, f"{heading2}", ln=2)
 
 
 
 
-	
 
 
 
@@ -34,4 +84,21 @@ for i in jsonData:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pdf.output("PANGILINAN_GABRIELLE.pdf")
 
